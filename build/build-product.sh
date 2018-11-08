ccfb6c9ec9d09ad89cabe93372981f6b1bd2c27a
fuzzywy <fuzzywy@163.com>
2018-11-08 09:43:12 +0800

    add cp command

--- build/build-product.sh
+++ build/build-product.sh
@@ -10,16 +10,31 @@ docker pull $composer
 
 docker pull $node
 
+# Build Bird project
 docker run -v ../Bird:/app --rm $composer composer install
 
+# Install Bird UI dependency
 docker run -v ../Bird:/home/node/app --rm -w /home/node/app $node npm install
 
+# Build Bird UI
 docker run -v ../Bird:/home/node/app --rm -w /home/node/app $node npm run production
 
+# Start Bird project
 docker-compose up -d
 
+# Specify access role for web root
 docker exec php-apache chmod 777 "-R" /var/www/html
 
+# Enviroment config
 docker exec php-apache cp .env.example .env
 
+# Key generate
 docker exec php-apache php artisan key:generate
+
+# Copy project into docker container
+cd Bird && docker cp . php-apache:/var/www/html
+
+# Commit container to image
+docker commit php-apache php-apache/genius
+
+# Push image to docker registry
