20301a6e3d7a0530c50fa10c5b5aabba9e8e9dc2
wangyang <fuzzywy@163.com>
2018-11-08 14:27:12 +0800

    Update build-product.sh

--- build/build-product.sh
+++ build/build-product.sh
@@ -3,26 +3,32 @@ node=node:10.7.0-stretch
 composer=composer
 prj=$PWD/Bird
 
+# Pull source code
 git clone https://eDeploy:ehub1234@github.com/fuzzywy/Bird.git
 
+# Build image
 docker-compose build
 
+# Pull composer image
 docker pull $composer
 
+# Pull node imge
 docker pull $node
 
 # Build Bird project
-
 docker run -v $prj:/app --rm $composer composer install
 
 # Install Bird UI dependency
 docker run -v $prj:/home/node/app --rm -w /home/node/app $node npm install
 
 # Build Bird UI
-docker run -v $prjBird:/home/node/app --rm -w /home/node/app $node npm run production
+docker run -v $prj/Bird:/home/node/app --rm -w /home/node/app $node npm run production
 
-# Start Bird project
-docker-compose up -d
+# Start php-apache image
+docker run --name php-apache php-apache/genius 
+
+# Copy project into docker container
+cd $prj && docker cp . php-apache:/var/www/html
 
 # Specify access role for web root
 docker exec php-apache chmod 777 "-R" /var/www/html
@@ -33,13 +39,10 @@ docker exec php-apache cp .env.example .env
 # Key generate
 docker exec php-apache php artisan key:generate
 
-# Copy project into docker container
-cd $prj && docker cp . php-apache:/var/www/html
-
 # Stop container
-docker-compose stop
+docker stop php-apache
 
-# Commit container to image
+# Commit changes to image
 docker commit php-apache php-apache/genius
 
 # Remove src dir
